5645075091b5d8abffcbcd5d2626056d513ac08c
Jphgamer14 <jacehungerford@yahoo.com>
2025-09-04 01:15:39 -0500

    Ready to test analogdial.sh

--- analogdial.sh
+++ analogdial.sh
@@ -10,8 +10,31 @@ apt-get install -y sudo ppp mgetty
 # Give my user the sudo group
 usermod -aG sudo nuso
 
-# Configure mgetty
+# Configure and start mgetty
 echo $mgetty >> /lib/systemd/system/mgetty@.service
 rm /etc/mgetty/mgetty.config
-echo 
+echo $modem >> /etc/mgetty/mgetty.config
+systemctl enable --now mgetty@ttyACM0.service
+
+# Setup and configure ppp
+rm /etc/ppp/options
+echo $ppp >> /etc/ppp/options
+echo $pppoptions >> /etc/ppp/options.ttyACM0
+
+# Setup the user for dialing in
+useradd -G dialout,dip,users -m -g users -s /usr/sbin/pppd dial
+echo "dial" | passwd --stdin "dial"
+echo "dial * "dial" *" >> /etc/ppp/pap-secrets
+
+# Setup rc-local file and service
+printf '%s\n' '#!/bin/bash' 'exit 0' | sudo tee -a /etc/rc.local
+chmod +x /etc/rc.local
+echo $rclocal >> /etc/systemd/system/rc-local.service
+systemctl enable rc-local
+
+# Configure ip forwarding and iptables
+sed -i 's/^#*net.ipv4.ip_forward *= *0/net.ipv4.ip_forward = 1/' /etc/sysctl.conf
+sysctl -p /etc/sysctl.conf
+echo "1" | update-alternatives --config iptables
+sed -i '1a\iptables -t nat -A POSTROUTING -s 192.168.32.0/24 -o ens18 -j MASQUERADE
 
--- variables.sh
+++ variables.sh
@@ -40,3 +40,36 @@ port ttyACM0
  toggle-dtr yes
  toggle-dtr-waittime 500
  rings 2"
+
+ ppp="# Define the DNS server for the client to use
+ms-dns 8.8.8.8
+# async character map should be 0
+asyncmap 0
+# Require authentication
+auth
+# Use hardware flow control
+crtscts
+# We want exclusive access to the modem device
+lock
+# Show pap passwords in log files to help with debugging
+show-password
+# Require the client to authenticate with pap
++pap
+# If you are having trouble with auth enable debugging
+debug
+# Heartbeat for control messages, used to determine if the client connection has dropped
+lcp-echo-interval 30
+lcp-echo-failure 4
+# Cache the client mac address in the arp system table
+proxyarp
+# Disable the IPXCP and IPX protocols.
+noipx"
+
+pppoptions="local
+lock
+nocrtscts
+192.168.1.200:192.168.1.201
+netmask 255.255.255.0
+noauth
+proxyarp
+lcp-echo-failure 60"
