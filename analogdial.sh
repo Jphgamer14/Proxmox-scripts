9dd3292f6482dec9aff75c74addb524cf77ed57b
Jphgamer14 <jacehungerford@yahoo.com>
2025-09-04 01:55:32 -0500

    Changed echo to printf

--- analogdial.sh
+++ analogdial.sh
@@ -11,31 +11,30 @@ apt-get install -y sudo ppp mgetty iptables
 usermod -aG sudo nuso
 
 # Configure and start mgetty
-echo $mgetty >> /lib/systemd/system/mgetty@.service
+printf $mgetty >> /lib/systemd/system/mgetty@.service
 rm /etc/mgetty/mgetty.config
-echo $modem >> /etc/mgetty/mgetty.config
+printf $modem >> /etc/mgetty/mgetty.config
 systemctl enable --now mgetty@ttyACM0.service
 
 # Setup and configure ppp
 rm /etc/ppp/options
-echo $ppp >> /etc/ppp/options
-echo $pppoptions >> /etc/ppp/options.ttyACM0
+printf $ppp >> /etc/ppp/options
+printf $pppoptions >> /etc/ppp/options.ttyACM0
 
 # Setup the user for dialing in
 useradd -G dialout,dip,users -m -g users -s /usr/sbin/pppd dial
-echo "dial:dial" | chpasswd
-echo "dial * "dial" *" >> /etc/ppp/pap-secrets
+printf "dial:dial" | chpasswd
+printf "dial * "dial" *" >> /etc/ppp/pap-secrets
 
 # Setup rc-local file and service
 printf '%s\n' '#!/bin/bash' 'exit 0' | sudo tee -a /etc/rc.local
 chmod +x /etc/rc.local
-echo $rclocal >> /etc/systemd/system/rc-local.service
+printf $rclocal >> /etc/systemd/system/rc-local.service
 systemctl enable rc-local
 
 # Configure ip forwarding and iptables
-sed -i 's/^#*net.ipv4.ip_forward *= *0/net.ipv4.ip_forward = 1/' /etc/sysctl.conf
+echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
 sysctl -p /etc/sysctl.conf
 echo "1" | update-alternatives --config iptables
 sed -i '1a\iptables -t nat -A POSTROUTING -s 192.168.32.0/24 -o ens18 -j MASQUERADE
 
-reboot
